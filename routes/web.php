1552e86974ee07de854aa3179be42005791d7001
Tony Ng <nguyencuonghut55@gmail.com>
2024-12-21 16:12:49 +0700

    Add login function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'home'])->name('name');
+/** Login routes */
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('show_login');
+Route::post('/login', [LoginController::class, 'handleLogin'])->name('login');
+
+Route::group(['middleware'=>'auth:web'], function() {
+    Route::get('/', [HomeController::class, 'home'])->name('name');
+});
