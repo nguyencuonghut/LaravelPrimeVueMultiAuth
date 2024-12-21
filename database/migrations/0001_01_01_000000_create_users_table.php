408f51cb1ba858a0ad84e94d00e9cfcd3cedad34
Tony Ng <nguyencuonghut55@gmail.com>
2024-12-21 15:17:25 +0700

    Add Role and Status columns to User table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['On', 'Off']);
+            $table->enum('role', ['Quản trị', 'Người dùng']);
             $table->rememberToken();
             $table->timestamps();
         });
